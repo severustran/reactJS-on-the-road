449214ef1387c438b3d4935f3daaab7e102751dc
Severus <thanhliem1923@gmail.com>
2019-11-04 00:36:36 +0700

    finish exercise 1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 class App extends Component {
   render() {
     var helloWorld = 'Welcome to the Road to learn React';
+    var myInfo = {
+      name: "Severus",
+      job: "Librarian"
+    }
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        <h2>My name is {myInfo.name}</h2>
+        <h2>My current job is {myInfo.job}</h2>
       </div>
     )
   }
