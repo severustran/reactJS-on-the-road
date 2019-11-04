ff2ed0692630d2f01cc38f5f3f473fe019e40d46
Severus <thanhliem1923@gmail.com>
2019-11-04 20:41:32 +0700

    nothing change

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
-    var myInfo = {
+    const helloWorld = 'Welcome to the Road to learn React';
+    let myInfo = {
       name: "Severus",
       job: "Librarian"
     }
     return (
       <div className="App">
+      <h2>{helloWorld}</h2>
         <h2>My name is {myInfo.name}</h2>
         <h2>My current job is {myInfo.job}</h2>
       </div>
